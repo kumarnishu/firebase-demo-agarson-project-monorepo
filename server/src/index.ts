a980d8b0e3424d99e8066dbba9f9b018ccb7c6b3
nishu kumar <kumarnishu437@gmail.com>
2023-07-30 10:12:35 +0530

    added firebase admin and removed firebase

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,27 @@
 import express from "express";
-
+import FireAdmin from "firebase-admin"
 const app = express()
+import dotenv from "dotenv";
+dotenv.config()
 
-app.listen(5000, () => console.log("Server is runing"))
+
+
+const ENV = process.env.NODE_ENV || "development"
+const PORT = Number(process.env.PORT) || 5000
+
+const admin = FireAdmin.initializeApp({
+    credential: FireAdmin.credential.cert(
+        {
+            projectId: process.env.projectId,
+            privateKey: process.env.privateKey,
+            clientEmail: process.env.clientEmail,
+        }
+    ),
+    databaseURL: "https://nodejs-blog-d85d2-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+admin.database().getRules().then((data) => {
+    console.log(data)
+})
+
+app.listen(PORT, () => console.log(`Server is running in ${ENV} mode at ${PORT}`))
